176e753bea51cd3aa36c005ccf87565fbae5c320
Mayan-Muhamed <muhamed.mayan28@gmail.com>
2023-08-15 20:16:03 +0600

    hw1

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import React from "react";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {seconds: 0}
+    }
+    tick(){
+        this.setState(state => ({
+            seconds: state.seconds + 1
+        }))
+    }
+    componentDidMount() {
+        this.interval = setInterval(() =>this.tick(), 1000)
+    }
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+    render() {
+        return (
+            <div>
+                seconds: {this.state.seconds}
+            </div>
+        )
+    }
+}
+
+export default App
